3b62fd99e79f24ef89c87c5744e0b9aca842f1c8
Anthony <anthonypm.29590@gmail.com>
2024-10-03 19:05:08 -0300

    created modal base for games screen

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,5 +1,29 @@
+'use client';
+import { useState } from 'react';
+
 export default function Games() {
-    return(
-        <div>Games</div>
-    );
+	const [modal, setModal] = useState(false);
+
+	function toggleModal() {
+		setModal(!modal);
+		console.log(modal);
+	}
+
+	return (
+		<>
+			<div id="modal-container" className={`${modal ? 'flex flex-col' : 'hidden'} modal-background fixed inset-x-0 inset-y-0 m-auto bg-gray-600 items-center`}>
+				<div id="modal" className="w-96 h-96 flex flex-col m-auto items-center bg-gray-800 rounded-xl justify-center">
+					<p>This will be my modal for my add/edit stuff</p>
+					<button onClick={toggleModal} className=" bg-gray-500">
+						Close modal
+					</button>
+				</div>
+			</div>
+			<div className="w-full h-full flex flex-col">
+				<button onClick={toggleModal} className="bg-gray-500 m-auto">
+					Open Modal
+				</button>
+			</div>
+		</>
+	);
 }
